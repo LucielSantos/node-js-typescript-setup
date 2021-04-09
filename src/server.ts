eb27d42b6ce2fdfd32353b522930b565f93c11d1
Luciel Santos <luciel.santos@ubistart.com>
2021-04-09 00:01:04 -0300

    config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello world' })
-});
+})
 
 app.listen(3333)
